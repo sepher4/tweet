65a0385a59e5286a8433964098920e14d6a848e2
Seth Chapa <seth.chapa@gmail.com>
2018-02-08 10:31:17 -0600

    tags work yo

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,8 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
+  include TweetsHelper
+
   # GET /tweets
   # GET /tweets.json
   def index
@@ -24,7 +26,10 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+
+    @tweet = Tweet.create(tweet_params)
+
+    @tweet = get_Tagged(@tweet)
 
     respond_to do |format|
       if @tweet.save
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,9 @@ class Tweet < ApplicationRecord
 
 	belongs_to :user
 
+	has_many :tweet_tags
+	has_many :tags, through: :tweet_tags
+
 	validates :message, presence: true
 
 	validates :message, length: {maximum: 140,
